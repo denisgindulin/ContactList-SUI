8dd71282e565dbda7af972289837c0354a774004
Denis Gindulin <denisgindulin@me.com>
2024-01-08 19:32:10 +0300

    Initial Commit

---
+++ ContactList-SUI/ContactList_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SUIApp.swift
+//  ContactList-SUI
+//
+//  Created by Денис Гиндулин on 08.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
